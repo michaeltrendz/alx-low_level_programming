82124d018a656fdedf74488a67b502ac60416497
michaeltrendz <daremichael07@gmail.com>
2023-07-15 19:49:48 -0700

    uodated

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print out with standard error.
+ * 
+ * Return - return 1
+*/
+
+int main(void)
+	{
+	write(2, "and that piece of art is useful" " - Dora Korpar, 2015-10-19\n", 58);
+	return 1;
+	}

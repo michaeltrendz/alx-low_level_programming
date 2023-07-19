6d612e752a652e3e21a9005434c8d610e3899814
michaeltrendz <daremichael07@gmail.com>
2023-07-18 18:40:16 -0700

    m

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+/**
+ * main - start here
+ * Return: 0
+ */
+int main(void)
+	{
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+
+		return (0);
+	}

55b52f3dae4aa478a2c08c76d4a589aa2704d3fc
michaeltrendz <daremichael07@gmail.com>
2023-07-20 17:54:52 -0700

    updated

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**i
+ *  main - tests functions that positive or negative
+ *  Return: always return 0
+*/
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
